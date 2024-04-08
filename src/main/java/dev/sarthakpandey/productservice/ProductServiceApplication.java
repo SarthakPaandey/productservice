0459a4d59a3759c37dbe90d919ea7420c1796f01
Sarthak Pandey <sarthakpandey973@gmail.com>
2024-04-08 21:17:57 +0530

    commit

--- src/main/java/dev/sarthakpandey/productservice/ProductServiceApplication.java
+++ src/main/java/dev/sarthakpandey/productservice/ProductServiceApplication.java
@@ -8,6 +8,9 @@ public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+        
+        
+
     }
 
 }
